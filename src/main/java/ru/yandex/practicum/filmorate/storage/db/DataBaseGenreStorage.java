041992c0103dd05d5ebf7eea385b48da184442fc
Tima <kulkukumber@yandex.ru>
2024-03-24 19:42:40 +0300

    checkStyle fix 2 : oh no

--- src/main/java/ru/yandex/practicum/filmorate/storage/db/DataBaseGenreStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/db/DataBaseGenreStorage.java
@@ -27,7 +27,7 @@ public class DataBaseGenreStorage implements GenreStorage {
     public List<FilmGenre> getAllGenre() {
         String sqlQuery = "SELECT * FROM genre";
 
-        return jdbcTemplate.query(sqlQuery, (rs, rowNum) -> сreateGenre(rs));
+        return jdbcTemplate.query(sqlQuery, (rs, rowNum) -> createGenre(rs));
     }
 
     @Override
@@ -45,7 +45,7 @@ public class DataBaseGenreStorage implements GenreStorage {
         }
     }
 
-    private FilmGenre сreateGenre(ResultSet rs) throws SQLException {
+    private FilmGenre createGenre(ResultSet rs) throws SQLException {
         return FilmGenre.builder()
                 .id(rs.getInt("genre_id"))
                 .name(rs.getString("name"))
